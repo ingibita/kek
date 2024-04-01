4998d07da651432d94d4246099bcc1bb67186486
q <q@q.q>
2024-04-01 17:17:03 +0300

    first commit

---
+++ app/src/main/java/com/example/ryazanceva/MainActivity1.java
@@ -0,0 +1,22 @@
+package com.example.ryazanceva;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.transition.Hold;
+
+public class MainActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void Tick(View v){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/ryazanceva/MainActivity2.java
@@ -0,0 +1,28 @@
+package com.example.ryazanceva;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.transition.Hold;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void Tick1(View v) {
+        Intent intent = new Intent(this, MainActivity4.class);
+        startActivity(intent);
+    }
+
+    public void Tick2(View v) {
+        Intent intent = new Intent(this, MainActivity3.class);
+        startActivity(intent);
+    }
+}
